8feae065579bfe917a29e312e1d1e9f55b9cfbe0
Caio Rodrigues <caicosta@uoldiveo.com>
2021-09-18 10:24:53 -0300

    Initial commit

---
+++ src/test/java/br/com/youtube/actuatordemo/ActuatorDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.youtube.actuatordemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActuatorDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
