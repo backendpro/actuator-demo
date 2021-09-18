92fea0ba311f0434ef96c309a6f7651153dbad5d
Caio Rodrigues <caicosta@uoldiveo.com>
2021-09-18 10:27:25 -0300

    Adicionado o hello world

--- src/main/java/br/com/youtube/actuatordemo/ActuatorDemoApplication.java
+++ src/main/java/br/com/youtube/actuatordemo/ActuatorDemoApplication.java
@@ -8,6 +8,7 @@ public class ActuatorDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ActuatorDemoApplication.class, args);
+		System.out.println("Hello, World!");
 	}
 
 }
